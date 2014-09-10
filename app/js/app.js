fb0fbae5f96312e49b39cff3f5d7950948e66dcf
Walker Malling <walker.malling@live.com>
2014-09-10 11:47:08 -0700

    move the calculation service to an app directive

--- app/js/app.js
+++ app/js/app.js
@@ -9,10 +9,17 @@ require('./calc/controllers/calc-controller')(mmmApp);
 mmmApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'views/index.html',
-      controller: 'calcController'
+      templateUrl: 'views/index.html'
     })
     .otherwise({
       redirectTo: '/'
     });
 }]);
+
+mmmApp.directive('calculate',function(){
+  return {
+    restrict: 'E',
+    templateUrl: '../views/calc/calc-view.html',
+    controller: 'calcController'
+  };
+});
